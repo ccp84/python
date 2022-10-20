8744ac6d2342c217196ad85843ccbf88c45054f3
ccp84 <ccp84@icloud.com>
2022-10-20 12:13:59 +0000

    Add function to collect sales data

--- run.py
+++ run.py
@@ -12,8 +12,18 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
 
-data = sales.get_all_values()
+def get_sales_data():
 
-print(data)
+    """
+    Get sales figures input from user
+    """
+    print("Please provide sales figures from last market")
+    print("Data should be 6 figures, separagted by commas")
+    print("Example 10, 20, 30, 40, 50, 60\n")
+
+    data_str = input("Enter sales figures here: ")
+    print(f"Data entered was {data_str}")
+
+
+get_sales_data()
